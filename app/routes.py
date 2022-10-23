10312776fa9c0fc4d60fa96632288a3396004fa7
Varun Srivastava <VarunSrivastava19ih@gmail.com>
2022-10-23 16:36:31 +0530

    Templates|Bootstrap

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,27 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Next Objective: Load some shitty website kek!'
+    user = {'username': 'Lil Jack'}
+    posts = [
+        {
+            'author': {'username': 'Roman'},
+            'body': 'LC is cool man!',
+        },
+        {
+            'author': {'username': 'Tony Prince'},
+            'body': 'LS is the way to go now!',
+        },
+        {
+            'author': {'username': 'Johny Klebitz'},
+            'body': 'LC Chapter is now done, LS is next. LOST FOREVER!',
+        },
+        {
+            'author': {'username': 'Pegerino'},
+            'body': 'LC is full of flying rats!',
+        },
+    ]
+    return render_template('index.html', user=user, posts=posts)
